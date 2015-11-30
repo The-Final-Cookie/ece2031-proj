b3a3caa48af811b1996cce820b60ac65e84432b8
tejaskhorana <tejaskhorana@gmail.com>
2015-11-29 20:42:02 -0500

    Update SCOMP.vhd

--- DE2Bot_v3_Fall15/SCOMP.vhd
+++ DE2Bot_v3_Fall15/SCOMP.vhd
@@ -90,7 +90,7 @@ BEGIN
 		wrcontrol_aclr_a => "NONE",
 		address_aclr_a   => "NONE",
 		outdata_aclr_a   => "NONE",
-		init_file        => "Motion.mif",
+		init_file        => "StartingCodeWithLog.mif",
 		lpm_hint         => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type         => "altsyncram"
 	)
